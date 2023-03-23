e9e81aa3f4001aa4fd5239f3a6156998abb2f5c6
quanghung97 <hungcan1997@gmail.com>
2023-03-23 10:26:42 +0700

    add config reading

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/quanghung97/gRPC-kafka-microservice/config"
+)
 
 func main() {
 	log.Println("Starting products service")
+
+	cfg, err := config.ParseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("REQUEST: %v", r.RemoteAddr)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(map[string]string{"message": "ok"}); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(cfg.Server.Port, nil))
 }
